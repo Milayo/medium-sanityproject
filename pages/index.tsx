7a64b5ddb34839bc57df7ed713a260b879312d27
Ijarotimi Tomilayo <moladeijarotimi@gmail.com>
2022-09-29 10:34:19 +0100

    blog section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,92 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import Header from "../components/header";
+import { sanityClient, urlFor } from "../sanity";
+import { Post } from "../typings";
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post];
+}
+
+const Home = ({ posts }: Props) => {
+  console.log(posts);
   return (
-    <div>
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <div>
-        <div>
-          <h1>Medium is a place to write, read and connect</h1>
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+        <div className="px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif">
+            <span className="underline decoration-black decoration-4">
+              Medium
+            </span>{" "}
+            is a place to write, read and connect
+          </h1>
           <h2>
             It is easy and free to post your thinking on any topic and connect
             with millions of readers.
           </h2>
         </div>
+        <img
+          className="hidden md:inline-flex h-32 lg:h-full"
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png "
+        />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="">
+              <img
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+                className="h-48 w-96 object-cover"
+              />
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p>{post.title}</p>
+                  <p>
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+                <img
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                  className="h-12 w-12 rounded-full object-cover"
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"] {
+  _id,
+  title,
+  author -> {
+  name,
+  image
+},
+description,
+mainImage,
+slug
+}
+  `;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
 export default Home;
